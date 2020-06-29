dbb945857c778b7446a3b2c0609b683bbbe6b450
Lola <yakjkf@gmail.com>
2020-06-29 11:58:52 +0600

    js code    banner background    numbers counter

--- src/js/index.js
+++ src/js/index.js
@@ -17,6 +17,53 @@
 
         });
 
+        let active = false;
+        const whyInfoBlock = document.querySelector('.why-info-bg-green');
+        console.log(whyInfoBlock);
+        if (whyInfoBlock) {
+            const numsCounter = function() {
+                if (!active) {
+
+                  //  setTimeout(() => {
+                        if (whyInfoBlock.getBoundingClientRect().top <= (window.innerHeight-100) && whyInfoBlock.getBoundingClientRect().bottom > 0) {
+                            active = true;
+                            const numbers = whyInfoBlock.querySelectorAll('.number');
+                            let cases = numbers[0];
+                            let percent = numbers[1];
+                            let years = numbers[2];
+                            cases.innerHTML = '';
+                            percent.innerHTML = '';
+                            years.innerHTML = '';
+
+                            let casesNums = 0;
+                            let casesInterval = setInterval(() => {
+                                casesNums++;
+                                cases.innerHTML = casesNums;
+                                if (casesNums >= 126) clearInterval(casesInterval);
+                            }, 0);
+
+                            let percentNums = 0;
+                            let percentInterval = setInterval(() => {
+                                percentNums++;
+                                percent.innerHTML = percentNums;
+                                if (percentNums >= 92) clearInterval(percentInterval);
+                            }, 5);
+
+                            let yearsNums = 0;
+                            let yearsInterval = setInterval(() => {
+                                yearsNums++;
+                                years.innerHTML = yearsNums;
+                                if (yearsNums >= 7) clearInterval(yearsInterval);
+                            }, 10);
+
+                        }
+                   // }, 200)
+                }
+            };
+
+            document.addEventListener('scroll', numsCounter);
+        }
+
     }, false);
 
     const show_menu = document.getElementById('show-menu');
@@ -31,5 +78,21 @@
     if (window.innerWidth <= 768) {
         const nav = document.querySelector('.header-bottom ');
         nav.append(social);
+    }
+
+    const banner = document.querySelector('.content-wrapper.head-banner');
+    if (banner) {
+        const bgs = ['img-e', 'img-s', 'img-f'];
+        let current = bgs[0];
+        let counter = 0;
+        let interval = setInterval(() => {
+            banner.classList.remove(current);
+            banner.classList.remove('visible');
+            counter++;
+            if (counter > 2) counter = 0;
+            current = bgs[counter];
+            banner.classList.add(current);
+            banner.classList.add('visible');
+        }, 5000);
     }
 })();
